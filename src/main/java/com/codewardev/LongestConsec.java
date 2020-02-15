3e9d813937e9a1688ae38e12b64a11dc55f0722e
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-15 18:21:04 +1100

    working on it

--- src/main/java/com/codewardev/LongestConsec.java
+++ src/main/java/com/codewardev/LongestConsec.java
@@ -3,7 +3,18 @@ package com.codewardev;
 public class LongestConsec {
 
 	public static String longestConsec(String[] strarr, int k) {
-		
+		StringBuilder result;
+		String output="";
+		for(int i=0; i<strarr.length; i++) {
+			result = new StringBuilder();
+			for(int j=i; j<i+k; j++) {
+				result.append(strarr[j]);
+			}
+			if(output.length() < result.toString().length()) {
+				output = result.toString();
+			}
+			System.out.println("Output: "+output);
+		}
 		return null;
 	}
 
