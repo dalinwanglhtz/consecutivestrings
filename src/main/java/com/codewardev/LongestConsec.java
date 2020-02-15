6c6839426479ed8d33589e086baf125d8286e8c7
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-15 22:21:55 +1100

    copied comparison code

--- src/main/java/com/codewardev/LongestConsec.java
+++ src/main/java/com/codewardev/LongestConsec.java
@@ -17,3 +17,21 @@ public class LongestConsec {
 	}
 
 }
+
+
+//package com.codewardev;
+//
+//import java.util.stream.Collectors;
+//import java.util.stream.IntStream;
+//
+//
+//public class LongestConsec {
+//	public static String longestConsec(String[] strArr, int k) {
+//		return (k>0 && strArr.length > 0 && k<= strArr.length) ?
+//				IntStream.rangeClosed(0, strArr.length-k)
+//				.mapToObj(i->IntStream.range(0, k).mapToObj(j->strArr[i+j]).collect(Collectors.joining()))
+//				.sorted((s1, s2)->Integer.compare(s2.length(), s1.length()))
+//				.findFirst().get()
+//				: "";
+//	}
+//}
