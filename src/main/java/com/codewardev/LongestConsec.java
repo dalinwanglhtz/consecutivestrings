cc74dc89043b170da0b28957ec27fccfcde61dd6
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-15 21:39:14 +1100

    refined working cut

--- src/main/java/com/codewardev/LongestConsec.java
+++ src/main/java/com/codewardev/LongestConsec.java
@@ -10,10 +10,9 @@ public class LongestConsec {
 		if(k <= 0) return output;
 		
 		for(int i=0; i<strarr.length; i++) {
-			if(i+k <= strarr.length) {
-				String test = String.join("", Arrays.copyOfRange(strarr, i, i+k));
-				output = output.length() < test.length() ? test: output;
-			}
+			if(i+k > strarr.length) break;
+			String test = String.join("", Arrays.copyOfRange(strarr, i, i+k));
+			output = output.length() < test.length() ? test: output;
 		}
 		return output;
 	}
